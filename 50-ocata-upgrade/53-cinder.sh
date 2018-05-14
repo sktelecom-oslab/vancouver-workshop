71c9c8a368f2a91d57849d851c19bea9319f8282
Jawon Choo <jawon.choo@sk.com>
2018-05-14 17:26:34 +0900

    add ocata update

--- 50-ocata-upgrade/53-cinder.sh
+++ 50-ocata-upgrade/53-cinder.sh
@@ -13,42 +13,44 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
 set -xe
 
-for JOBS in keystone-db-init keystone-db-sync keystone-bootstrap keystone-credential-setup keystone-domain-manage keystone-fernet-setup; do
+for JOBS in cinder-backup-storage-init cinder-bootstrap cinder-db-init cinder-db-sync cinder-ks-endpoints cinder-ks-service cinder-ks-user cinder-storage-init; do
   kubectl --namespace openstack delete job "${JOBS}"
 done
 
-tee /tmp/keystone-ocata.yaml <<EOF
+tee /tmp/cinder-ocata.yaml <<EOF
 images:
   tags:
-    bootstrap: docker.io/openstackhelm/heat:ocata
     test: docker.io/kolla/ubuntu-source-rally:ocata
     db_init: docker.io/openstackhelm/heat:ocata
-    keystone_db_sync: docker.io/openstackhelm/keystone:ocata
+    cinder_db_sync: docker.io/openstackhelm/cinder:ocata
     db_drop: docker.io/openstackhelm/heat:ocata
-    ks_user: docker.io/openstackhelm/heat:ocata
     rabbit_init: docker.io/rabbitmq:3.7-management
-    keystone_fernet_setup: docker.io/openstackhelm/keystone:ocata
-    keystone_fernet_rotate: docker.io/openstackhelm/keystone:ocata
-    keystone_credential_setup: docker.io/openstackhelm/keystone:ocata
-    keystone_credential_rotate: docker.io/openstackhelm/keystone:ocata
-    keystone_api: docker.io/openstackhelm/keystone:ocata
-    keystone_domain_manage: docker.io/openstackhelm/keystone:ocata
+    ks_user: docker.io/openstackhelm/heat:ocata
+    ks_service: docker.io/openstackhelm/heat:ocata
+    ks_endpoints: docker.io/openstackhelm/heat:ocata
+    cinder_api: docker.io/openstackhelm/cinder:ocata
+    bootstrap: docker.io/openstackhelm/heat:ocata
+    cinder_scheduler: docker.io/openstackhelm/cinder:ocata
+    cinder_volume: docker.io/openstackhelm/cinder:ocata
+    cinder_volume_usage_audit: docker.io/openstackhelm/cinder:ocata
+    cinder_storage_init: docker.io/port/ceph-config-helper:v1.10.2
+    cinder_backup: docker.io/openstackhelm/cinder:ocata
+    cinder_backup_storage_init: docker.io/port/ceph-config-helper:v1.10.2
     dep_check: quay.io/stackanetes/kubernetes-entrypoint:v0.3.1
     image_repo_sync: docker.io/docker:17.07.0
 bootstrap:
   enabled: false
 EOF
-helm upgrade keystone ~/vancouver-workshop/openstack-helm/keystone \
-    -f /tmp/keystone-ocata.yaml
+helm upgrade cinder ~/vancouver-workshop/openstack-helm/cinder \
+    -f /tmp/cinder-ocata.yaml
 
 #NOTE: Wait for deploy
 bash ~/vancouver-workshop/90-common/wait-for-pods.sh openstack
 
 #NOTE: Validate Deployment info
-helm status keystone
 export OS_CLOUD=openstack_helm
+openstack service list
 sleep 30 #NOTE(portdirect): Wait for ingress controller to update rules and restart Nginx
-openstack endpoint list
+openstack volume type list
