ab9f4019fd38d4421ae72b891d1e0f515711d055
Siri Kim <siri.kim1012@gmail.com>
2018-05-15 02:05:43 +0000

    add remove-all file

---
+++ 90-common/remove-all.sh
@@ -0,0 +1,11 @@
+for NS in openstack ceph nfs libvirt; do 
+   helm ls --namespace $NS --short | xargs -L1 -P2 helm delete --purge;
+done;
+sudo systemctl stop kubelet;
+sudo systemctl disable kubelet;
+sudo docker ps -aq | xargs -L1 -P16 sudo docker rm -f;
+sudo rm -rf /var/lib/openstack-helm/*;
+sudo rm -rf /var/lib/nova/*;
+sudo rm -rf /var/lib/libvirt/*;
+sudo rm -rf /etc/libvirt/qemu/*;
+sudo findmnt --raw | awk '/^\/var\/lib\/kubelet\/pods/ { print $1 }' | xargs -L1 -P16 sudo umount -f -l;
