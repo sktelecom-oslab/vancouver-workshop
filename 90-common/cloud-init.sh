e6da8eb9c117fe8d4a21cea9d8a481fb56041f3d
Jawon Choo <jawon.choo@sk.com>
2018-05-19 21:44:45 +0900

    change dir

--- 90-common/cloud-init.sh
+++ 90-common/cloud-init.sh
@@ -5,10 +5,11 @@ echo "ubuntu:openstackhelm" | sudo -H chpasswd
 sudo -H sed -i 's|PasswordAuthentication no|PasswordAuthentication yes|g' /etc/ssh/sshd_config
 sudo -H systemctl restart sshd
 
-sudo -H su -c 'git clone https://github.com/sktelecom-oslab/vancouver-workshop.git /home/ubuntu/vancouver-workshop' ubuntu
-sudo -H su -c 'git clone https://git.openstack.org/openstack/openstack-helm /home/ubuntu/vancouver-workshop/openstack-helm' ubuntu
-sudo -H su -c 'git clone https://git.openstack.org/openstack/openstack-helm-infra /home/ubuntu/vancouver-workshop/openstack-helm-infra' ubuntu
-sudo -H su -c 'cd /home/ubuntu/vancouver-workshop/openstack-helm; ./tools/deployment/developer/common/000-install-packages.sh; ./tools/deployment/developer/common/000-install-packages.sh; ./tools/deployment/developer/common/010-deploy-k8s.sh; ./tools/deployment/developer/common/020-setup-client.sh' ubuntu
-sudo -H su -c '(cd /home/ubuntu/vancouver-workshop/openstack-helm; make all pull-all-images)' ubuntu
-sudo -H su -c '(cd /home/ubuntu/vancouver-workshop/openstack-helm-infra; make all pull-all-images)' ubuntu
-sudo -H su -c '(/home/ubuntu/vancouver-workshop/90-common/pull-ocata-images.sh)' ubuntu
+sudo -H su -c 'git clone https://github.com/sktelecom-oslab/vancouver-workshop.git /opt/vancouver-workshop' ubuntu
+sudo -H su -c 'git clone https://git.openstack.org/openstack/openstack-helm /opt/openstack-helm' ubuntu
+sudo -H su -c 'git clone https://git.openstack.org/openstack/openstack-helm-infra /opt/openstack-helm-infra' ubuntu
+
+sudo -H su -c 'cd /opt/openstack-helm; ./tools/deployment/developer/common/000-install-packages.sh; ./tools/deployment/developer/common/000-install-packages.sh; ./tools/deployment/developer/common/010-deploy-k8s.sh; ./tools/deployment/developer/common/020-setup-client.sh' ubuntu
+sudo -H su -c '(cd /opt/openstack-helm; make all pull-all-images)' ubuntu
+sudo -H su -c '(cd /opt/openstack-helm-infra; make all pull-all-images)' ubuntu
+sudo -H su -c '(/opt/vancouver-workshop/90-common/pull-ocata-images.sh)' ubuntu
