674a0b4880bb9bd3e545629401f5d5dd250a0d81
Siri Kim <siri.kim1012@gmail.com>
2018-05-21 08:35:46 +0000

    add vm creation for ocata

--- 90-common/910-use-for-ocata.sh
+++ 90-common/910-use-for-ocata.sh
@@ -1,10 +1,10 @@
+WORK_DIR=/opt/openstack-helm
+
 export OS_CLOUD=openstack_helm
 export OSH_EXT_NET_NAME="public"
 export OSH_VM_FLAVOR="m1.tiny"
 export OSH_VM_KEY_STACK="heat-vm-key"
 export OSH_PRIVATE_SUBNET="10.0.0.0/24"
-
-WORK_DIR=/opt/openstack-helm
 export IMAGE_NAME=$(openstack image show -f value -c name \
   $(openstack image list -f csv | awk -F ',' '{ print $2 "," $1 }' | \
     grep "^\"Cirros" | head -1 | awk -F ',' '{ print $2 }' | tr -d '"'))
